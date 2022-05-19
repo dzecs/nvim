1c39ca275b794c5054bdaa35535b8c74c78b91a4
Diego Ezequiel <diego.scardulla@outlook.com>
2022-05-18 21:29:51 -0300

    better lualine

--- lua/zxc/core/lualine.lua
+++ lua/zxc/core/lualine.lua
@@ -12,8 +12,8 @@ lualine.setup({
 	options = {
 		icons_enabled = true,
 		theme = "catppuccin",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {},
 		always_divide_middle = true,
 	},
@@ -21,8 +21,8 @@ lualine.setup({
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
 		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_y = { "progress" },
+		lualine_x = {},
+		lualine_y = {},
 		lualine_z = { "location" },
 	},
 	inactive_sections = {
